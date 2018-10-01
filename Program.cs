c5afc2afb76d536115a570f440ead6a4eb152c7d
BrianHo <Curry30@Brians-MacBook-Pro.local>
2018-10-01 16:06:14 -0700

    first push

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            List<object> ListObject = new List<object>();
+            ListObject.Add(7);
+            ListObject.Add(28);
+            ListObject.Add(-1);
+            ListObject.Add(true);
+            ListObject.Add("chair");
+            for (int i = 0; i < ListObject.Count; i++)
+            {
+                System.Console.Write(ListObject[i] +" ");
+                if(ListObject[i] is int){
+                    sum = sum + (int)ListObject[i];
+                }
+            }
+            System.Console.WriteLine("\n" + sum);
+            
+            // object value = 5;
+            // if(value is int){
+            //     System.Console.WriteLine("it works");
+
+            // }else{
+            //     System.Console.WriteLine("not working");
+            // }
+        }
+    }
+}
